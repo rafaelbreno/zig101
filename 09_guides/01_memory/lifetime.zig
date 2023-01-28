9eca84967e3cdff3a24891f9809ef4874d328457
Rafael Breno <rafael_breno@outlook.com>
2023-01-28 17:46:20 -0300

    somethign

--- 09_guides/01_memory/lifetime.zig
+++ 09_guides/01_memory/lifetime.zig
@@ -3,7 +3,6 @@ const print = std.debug.print;
 
 pub fn main() !void {
     var alloc = std.heap.GeneralPurposeAllocator(.{}){};
-    alloc.setRequestedMemoryLimit(5);
     var array = std.ArrayList(u8).init(alloc.allocator());
     defer _ = alloc.deinit();
     defer array.deinit();
@@ -14,10 +13,9 @@ pub fn main() !void {
     print("\t    cap: {d}\n", .{array.capacity});
     //const ptr = &array;
     print("\taddress: {*}\n", .{&(array.items)});
-    print("\taddress: {*}\n", .{&(array)});
 
     var i: u8 = 0;
-    while (i < 100) {
+    while (i < 10) {
         try array.append(i);
         i += 1;
     }
@@ -26,5 +24,4 @@ pub fn main() !void {
     print("\t    val: {any}\n", .{array.items});
     print("\t    cap: {d}\n", .{array.capacity});
     print("\taddress: {*}\n", .{&(array.items)});
-    print("\taddress: {*}\n", .{&(array)});
 }
